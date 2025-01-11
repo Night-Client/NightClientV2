f1a39803fc066da36ad336b38e3b24e26fcb2f04
Night-Client <134009215+Night-Client@users.noreply.github.com>
2025-01-10 21:32:56 -0700

    Update NoFall.java

--- src/main/java/booogyboooo/nightclientv2/modules/misc/NoFall.java
+++ src/main/java/booogyboooo/nightclientv2/modules/misc/NoFall.java
@@ -3,14 +3,17 @@ package booogyboooo.nightclientv2.modules.misc;
 import booogyboooo.nightclientv2.event.module.ModuleEvents;
 import booogyboooo.nightclientv2.module.Module;
 import booogyboooo.nightclientv2.module.ModuleType.Type;
+import booogyboooo.nightclientv2.util.BlockPlacePacketUtil;
 import booogyboooo.nightclientv2.util.Key;
 import booogyboooo.nightclientv2.util.MovePacketUtil;
+import booogyboooo.nightclientv2.util.PlayerPosUtil;
 
 public class NoFall extends Module implements ModuleEvents {
 
 	public NoFall() {
 		super(Type.Misc, "NoFall", "Vannila", Key.NONE.key());
 		this.addMode("Vannila");
+		this.addMode("AirPlace");
 	}
 	
 	@Override
@@ -18,7 +21,14 @@ public class NoFall extends Module implements ModuleEvents {
 		if (!this.isToggled()) {
 			return;
 		}
-		MovePacketUtil.onGround(true);
+		if (this.getMode().equals("Vannila")) {
+			MovePacketUtil.onGround(true);
+		}
+		if (this.getMode() == "AirPlace") {
+			if (mc.player.fallDistance > 2.5) {
+				BlockPlacePacketUtil.placeBlockVannila(PlayerPosUtil.getBlockUnderPlayer(mc, true));
+			}
+		}
 	}
 
 }
